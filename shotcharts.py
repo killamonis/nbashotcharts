5db0ef2cb6848e5bcf8bf8d0125cc4a36d62fe42
Monis Mohiuddin <monmohiuddin@berkeley.edu>
2021-03-03 18:48:13 -0800

    progress made on updated shot chart

--- shotcharts.py
+++ shotcharts.py
@@ -33,6 +33,7 @@ def get_player_id(first, last):
             return p['playerId']
     return -1
 
+#Gets the specific shot zone of an individual field goal attempt
 def get_shot_zone(x, y):
     d = np.sqrt(x**2 + y**2)
     z = np.arctan2(y,x) * 180/np.pi
@@ -103,7 +104,6 @@ def create_court(ax=None, color='black', lw=4):
         ax.add_patch(element)
     return ax
 
-print(get_player_id('Kyle', 'Kuzma'))
 shotchart,league_average = nba.shotchart.shotchartdetail(playerid = get_player_id('Kyle', 'Kuzma'), season = '2019-20')
 league = league_average.loc[:,'SHOT_ZONE_AREA':'FGM'].groupby(['SHOT_ZONE_RANGE','SHOT_ZONE_AREA']).sum()
 league['FGP'] = 1.0 * league['FGM']/league['FGA']
@@ -148,68 +148,4 @@ p = mpl.PatchCollection(patches,cmap=color_map,alpha=1)
 p.set_array(np.array(colors))
 ax.add_collection(p)
 p.set_clim([0, len(bins)-1])
-
-# JSON request for 2020 Kuz
-''' shot_json = shotchartdetail.ShotChartDetail(
-    team_id = get_team_id('los angeles lakers'),
-    player_id = get_player_id('kyle', 'kuzma'),
-    context_measure_simple = 'FGA', 
-    season_nullable = '2019-20',
-    season_type_all_star = 'Regular Season')
-
-# Converting data into dictionary, getting relevant data
-shot_data = json.loads(shot_json.get_json())
-printTest = json.dumps(shot_data, indent = 2)
-with open('kuz.json', 'w') as f:
-    json.dump(shot_data, f, indent = 2) '''
-
-
-
-''' relevant_data = shot_data['resultSets'][0]
-headers = relevant_data['headers']
-rows = relevant_data['rowSet']
-
-# Create pandas data frame
-kuz_data = pd.DataFrame(rows)
-kuz_data.columns = headers
-
-# Drawing basketball court
-def create_court(ax, color):
-    # 3 point line
-    ax.plot([-220, -220], [0, 140], linewidth = 2, color = color)
-    ax.plot([220, 220], [0, 140], linewidth = 2, color = color)
-    ax.add_artist(mpl.patches.Arc((0, 140), 440, 315, theta1=0, theta2=180, facecolor='none', edgecolor=color, lw=2))
-
-    # Key and lane
-    ax.plot([-80, -80], [0, 190], linewidth=2, color=color)
-    ax.plot([80, 80], [0, 190], linewidth=2, color=color)
-    ax.plot([-60, -60], [0, 190], linewidth=2, color=color)
-    ax.plot([60, 60], [0, 190], linewidth=2, color=color)
-    ax.plot([-80, 80], [190, 190], linewidth=2, color=color)
-    ax.add_artist(mpl.patches.Circle((0, 190), 60, facecolor='none', edgecolor=color, lw=2))
-
-    # Rim and backboard
-    ax.add_artist(mpl.patches.Circle((0, 60), 15, facecolor='none', edgecolor=color, lw=2))
-    ax.plot([-30, 30], [40, 40], linewidth=2, color=color)
-
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_xlim(-250, 250)
-    ax.set_ylim(0, 470)
-
-    return ax
-
-mpl.rcParams['font.family'] = 'Avenir'
-mpl.rcParams['font.size'] = 18
-mpl.rcParams['axes.linewidth'] = 2
-
-# Draw court
-fig = plt.figure(figsize=(4, 3.76))
-ax = fig.add_axes([0, 0, 1, 1])
-ax = create_court(ax, 'black')
-
-ax.hexbin(kuz_data['LOC_X'], kuz_data['LOC_Y'] + 60, gridsize=(30, 30), extent=(-300, 300, 0, 940), bins='log', cmap='Purples')
-
-plt.savefig('ShotChart1.png', dpi=300, bbox_inches='tight')
-plt.show() '''
 
